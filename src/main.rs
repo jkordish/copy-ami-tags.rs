66c4575a44403eaaa7cba190347add20d9005d6f
Joseph Kordish <joseph.kordish@mandiant.com>
2017-11-18 14:56:57 -0600

    copy-ami-tags.rs

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ use serde_json::Value;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    if args.len() < 2 {
-        println!("usage: copy-ami-tags-rs <role_name> <source_account> <shared_account,shared_account>");
+    if args.len() < 3 {
+        eprintln!("usage: copy-ami-tags-rs <role_name> <source_account> <shared_account,shared_account>");
     }
 
     let role_name = &args[1];
