3f068aa3a3bad6739b045b86c1d5286317f4063b
Joseph Kordish <joseph.kordish@mandiant.com>
2017-11-19 16:28:10 -0600

    optimizations

--- src/main.rs
+++ src/main.rs
@@ -36,156 +36,157 @@ fn main() {
     // open our config file
     let packer_manifest = match File::open(&"manifest.json") {
         Ok(file) => file,
-        Err(_) => { println!("Unable to open manifest.json"); exit(1) }
+        Err(_) => {
+            println!("Unable to open manifest.json");
+            exit(1)
+        }
     };
 
     // attempt to deserialize the config to our struct
     let manifest: Value = match serde_json::from_reader(packer_manifest) {
         Ok(json) => json,
-        Err(_) => { println!("manifest.json not valid json?"); exit(1) }
+        Err(_) => {
+            println!("manifest.json not valid json?");
+            exit(1)
+        }
     };
 
     // grab the list of artifacts
     let artifacts = match manifest["builds"][0]["artifact_id"].as_str() {
         Some(artifacts) => artifacts,
-        _ => { println!("No artifacts present in manifest"); exit(1) }
+        _ => {
+            println!("No artifacts present in manifest");
+            exit(1)
+        }
     };
 
     // loop through each region:ami pair
-    for artifact in artifacts.split(',') {
-        let pair: Vec<&str> = artifact.split(':').collect();
-        scope(|scope| {
+    scope(|scope| {
+        for artifact in artifacts.split(',') {
+            let pair: Vec<&str> = artifact.split(':').collect();
             scope.spawn(move || {
-                logging(&format!("Processing AMI {} in REGION {} for ACCOUNT {}", &pair[0], &pair[1], &source_account));
                 source_ami(role_name, source_account, shared_account, pair[0], pair[1]);
+                logging(&format!("Processing AMI {} in REGION {} for ACCOUNT {}", &pair[0], &pair[1], &source_account));
             });
-        });
-    }
+        }
+    });
 }
 
 fn source_ami(role: &str, source_account: &str, shared_account: &[&str], region: &str, ami: &str) {
-    scope(|scope| {
-        scope.spawn(move || {
-            // create our role_name from the source account and provided role name
-            let role_name = format!("arn:aws:iam::{}:role/{}", source_account, role);
+    // create our role_name from the source account and provided role name
+    let role_name = format!("arn:aws:iam::{}:role/{}", source_account, role);
 
-            // set up our credentials provider for aws
-            let provider = match DefaultCredentialsProvider::new() {
-                Ok(provider) => provider,
-                Err(err) => panic!("Unable to load credentials. {}", err)
-            };
+    // set up our credentials provider for aws
+    let provider = match DefaultCredentialsProvider::new() {
+        Ok(provider) => provider,
+        Err(err) => panic!("Unable to load credentials. {}", err)
+    };
 
-            // initiate our sts client
-            let sts_client = StsClient::new(
-                default_tls_client().unwrap(),
-                provider,
-                Region::from_str(region).unwrap()
-            );
+    // initiate our sts client
+    let sts_client = StsClient::new(
+        default_tls_client().unwrap(),
+        provider,
+        Region::from_str(region).unwrap()
+    );
 
-            // generate a sts provider
-            let sts_provider = StsAssumeRoleSessionCredentialsProvider::new(
-                sts_client,
-                role_name.to_owned(),
-                "packer-api".to_owned(),
-                None,
-                None,
-                None,
-                None
-            );
-            // allow our STS to auto-refresh
-            let auto_sts_provider = AutoRefreshingProvider::with_refcell(sts_provider).unwrap();
+    // generate a sts provider
+    let sts_provider = StsAssumeRoleSessionCredentialsProvider::new(
+        sts_client,
+        role_name.to_owned(),
+        "packer-api".to_owned(),
+        None,
+        None,
+        None,
+        None
+    );
 
-            // create our ec2 client initialization
-            let client = Ec2Client::new(
-                default_tls_client().unwrap(),
-                auto_sts_provider,
-                Region::from_str(region).unwrap()
-            );
+    // allow our STS to auto-refresh
+    let auto_sts_provider = AutoRefreshingProvider::with_refcell(sts_provider).unwrap();
 
-            // create our filter for the source ami
-            let filter = Filter { name: Some("resource-id".to_string()), values: Some(vec![ami.to_owned()]) };
+    // create our ec2 client initialization
+    let client = Ec2Client::new(
+        default_tls_client().unwrap(),
+        auto_sts_provider,
+        Region::from_str(region).unwrap()
+    );
 
-            // create our request
-            let tags_request = DescribeTagsRequest { filters: Some(vec![filter]), ..Default::default() };
+    // create our filter for the source ami
+    let filter = Filter { name: Some("resource-id".to_string()), values: Some(vec![ami.to_owned()]) };
 
+    // create our request
+    let tags_request = DescribeTagsRequest { filters: Some(vec![filter]), ..Default::default() };
 
-            logging(&format!("Requesting tags from AMI {} in REGION {} for ACCOUNT {}", &ami, &region, &source_account));
+    logging(&format!("Requesting tags from AMI {} in REGION {} for ACCOUNT {}", &ami, &region, &source_account));
 
-            // grab those tags and attempt to unwrap them
-            // if successful, then send those tags to the dest ami
-            match client.describe_tags(&tags_request) {
-                Ok(_src_ami) => destination_ami(region, ami, shared_account, role, &_src_ami.tags.unwrap()),
-                Err(e) =>  { logging(&format!("Unable to collect tags for {} Error: {:?}", ami, e)); exit(1) }
-            };
-        });
-    });
+    // grab those tags and attempt to unwrap them
+    // if successful, then send those tags to the dest ami
+    match client.describe_tags(&tags_request) {
+        Ok(_src_ami) => destination_ami(region, ami, shared_account, role, &_src_ami.tags.unwrap()),
+        Err(e) => {
+            logging(&format!("Unable to collect tags for {} Error: {:?}", ami, e));
+            exit(1)
+        }
+    };
 }
 
 fn destination_ami(region: &str, ami: &str, shared_account: &[&str], role: &str, source_ami_tags: &[TagDescription]) {
     for account in shared_account {
         let source_ami_tags: Vec<_> = source_ami_tags.to_owned();
-        scope(|scope| {
-            scope.spawn(move || {
-                logging(&format!("Applying tags to AMI {} in REGION {} for ACCOUNT {}", &ami, &region, &account));
-                // create our role_name from account and provided name
-                let role_name = format!("arn:aws:iam::{}:role/{}", account, role);
+        logging(&format!("Applying tags to AMI {} in REGION {} for ACCOUNT {}", &ami, &region, &account));
+        // create our role_name from account and provided name
+        let role_name = format!("arn:aws:iam::{}:role/{}", account, role);
 
-                // set up our credentials provider for aws
-                let provider = match DefaultCredentialsProvider::new() {
-                    Ok(provider) => provider,
-                    Err(err) => panic!("Unable to load credentials. {}", err)
-                };
+        // set up our credentials provider for aws
+        let provider = match DefaultCredentialsProvider::new() {
+            Ok(provider) => provider,
+            Err(err) => panic!("Unable to load credentials. {}", err)
+        };
 
-                // initiate our sts client
-                let sts_client = StsClient::new(
-                    default_tls_client().unwrap(),
-                    provider,
-                    Region::from_str(region).unwrap()
-                );
+        // initiate our sts client
+        let sts_client = StsClient::new(
+            default_tls_client().unwrap(),
+            provider,
+            Region::from_str(region).unwrap()
+        );
 
-                // generate a sts provider
-                let sts_provider = StsAssumeRoleSessionCredentialsProvider::new(
-                    sts_client,
-                    role_name.to_owned(),
-                    "packer-api".to_owned(),
-                    None,
-                    None,
-                    None,
-                    None
-                );
-                // allow our STS to auto-refresh
-                let auto_sts_provider = AutoRefreshingProvider::with_refcell(sts_provider).unwrap();
+        // generate a sts provider
+        let sts_provider = StsAssumeRoleSessionCredentialsProvider::new(
+            sts_client,
+            role_name.to_owned(),
+            "packer-api".to_owned(),
+            None,
+            None,
+            None,
+            None
+        );
 
-                // create our ec2 client initialization
-                let client = Ec2Client::new(
-                    default_tls_client().unwrap(),
-                    auto_sts_provider,
-                    Region::from_str(region).unwrap()
-                );
+        // allow our STS to auto-refresh
+        let auto_sts_provider = AutoRefreshingProvider::with_refcell(sts_provider).unwrap();
 
-                // create mutable vec of our source ami tags
-                let mut tags_buff: Vec<_> = vec![];
+        // create our ec2 client initialization
+        let client = Ec2Client::new(
+            default_tls_client().unwrap(),
+            auto_sts_provider,
+            Region::from_str(region).unwrap()
+        );
 
-                // loop through the tags from our source ami and add it to our mutable tags_buff vec
-                for tag in source_ami_tags {
-                    tags_buff.push(Tag { key: Some(tag.key.unwrap().to_string()), value: Some(tag.value.unwrap().to_string()) })
-                }
+        // create mutable vec of our source ami tags
+        let mut tags: Vec<_> = vec![];
 
-                // create a CreateTagsRequest
-                let tag_request = CreateTagsRequest { resources: vec![ami.to_owned()], tags: tags_buff, ..Default::default() };
+        // loop through the tags from our source ami and add it to our mutable tags_buff vec
+        for tag in source_ami_tags {
+            tags.push(Tag { key: Some(tag.key.unwrap().to_string()), value: Some(tag.value.unwrap().to_string()) })
+        }
 
-                // apply tags
-                match client.create_tags(&tag_request) {
-                    Ok(_) => {
-                        logging(&format!("Successfully copied tags to AMI {} in ACCOUNT {} for REGION {}", ami, account, region))
-                    },
-                    Err(e) => {
-                        logging(&format!("Unsuccessful in copying tags from AMI {} to ACCOUNT {} for REGION {} {:?}", ami, account, region, e));
-                        exit(1)
-                    }
-                }
-            });
-        });
+        // create a CreateTagsRequest
+        let tag_request = CreateTagsRequest { resources: vec![ami.to_owned()], tags, ..Default::default() };
+
+        // apply tags
+        if client.create_tags(&tag_request).is_ok() {
+            logging(&format!("Successfully copied tags to AMI {} in ACCOUNT {} for REGION {}", ami, account, region))
+        } else {
+            logging(&format!("Unsuccessful in copying tags from AMI {} to ACCOUNT {} for REGION {}", ami, account, region))
+        };
     }
 }
 
@@ -195,5 +196,5 @@ fn logging(msg: &str) {
     let drain = slog_async::Async::new(drain).build().fuse();
 
     let logger = slog::Logger::root(drain, o!());
-    info!(logger, "copy-ami-tags"; "message:" => &msg);
+    info!(logger, "copy-ami-tags"; "[*]" => &msg)
 }
